4cb71ad678761240fe4a8c278b73d216a53b4e02
Qian, Yi <yi.qian@intel.com>
2026-02-25 12:45:42 +0800

    fix moe expert computation time calculation

--- backend/model_analyzer.py
+++ backend/model_analyzer.py
@@ -516,6 +516,9 @@ class MoEAnalyzer(ModelAnalyzer):
         num_key_value_heads = self.module.get_num_key_value_heads(model_params)
         num_hidden_layers = self.module.get_num_hidden_layers(model_params)
         num_active_experts = self.module.get_num_active_experts(model_params) // tp_size
+
+        expert_diff_ratio = 0.3
+
         for name, (ic, oc) in self.module.get_linear_layers(model_params, tp_size).items():
             # for linear layers
             is_kv_proj = name in ["k_proj", "v_proj"]
@@ -527,7 +530,7 @@ class MoEAnalyzer(ModelAnalyzer):
                     "decode",
                     name,
                     OPs=ic * oc * batchsize * 2 * num_active_experts,
-                    load_weight=ic * oc * w_byte * num_active_experts,
+                    load_weight=ic * oc * w_byte * batchsize * num_active_experts * expert_diff_ratio,
                     load_act=ic * batchsize * a_byte * num_active_experts,
                     store_act=oc * batchsize * a_byte * num_active_experts,
                     load_kv_cache=0,
@@ -538,7 +541,7 @@ class MoEAnalyzer(ModelAnalyzer):
                     "prefill",
                     name,
                     OPs=ic * oc * batchsize * seqlen * 2 * num_active_experts,
-                    load_weight=ic * oc * w_byte * num_active_experts,
+                    load_weight=ic * oc * w_byte * batchsize * num_active_experts * expert_diff_ratio,
                     load_act=ic * batchsize * seqlen * a_byte * num_active_experts,
                     store_act=oc * batchsize * seqlen * a_byte * num_active_experts,
                     load_kv_cache=0,
