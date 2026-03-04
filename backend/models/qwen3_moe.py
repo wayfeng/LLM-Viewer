d23bf52f05e90aae2009f445094e41adb412642a
Wayne Feng <wei.feng@intel.com>
2026-03-04 11:11:38 +0800

    add moe gate

--- backend/models/qwen3_moe.py
+++ backend/models/qwen3_moe.py
@@ -54,6 +54,7 @@ def post_process(model_params,args):
     return layers
 
 def get_linear_layers(model_params, tp_size: int):
+    num_experts = get_num_experts(model_params)
     hidden_size=get_hidden_size(model_params)
     intermediate_size=get_intermediate_size(model_params)
     key_value_heads=get_num_key_value_heads(model_params)
@@ -70,7 +71,7 @@ def get_linear_layers(model_params, tp_size: int):
         "k_proj":[hidden_size, hidden_size * key_value_heads // attention_heads // tp_size],
         "v_proj":[hidden_size, hidden_size * key_value_heads // attention_heads // tp_size],
         "out_proj":[hidden_size // tp_size, hidden_size],
-	"gate": [hidden_size, num_experts],
+	    "gate": [hidden_size, num_experts],
         "gate_proj":[hidden_size, moe_intermediate_size],
         "up_proj":[hidden_size, moe_intermediate_size],
         "down_proj":[moe_intermediate_size, hidden_size],
@@ -88,7 +89,9 @@ transformer_layer_graph={
     "sv_matmul":["softmax","v_proj"],
     "out_proj":["sv_matmul"],
     "attn_add":["input","out_proj"],
-    "mlp_norm":["attn_add"],
+    "post_attn_norm":["attn_add"],
+    "gate":["post_attn_norm"],
+    "mlp_norm":["gate"],
     "gate_proj":["mlp_norm"],
     "up_proj":["mlp_norm"],
     "mlp_act":["gate_proj", "up_proj"],
@@ -106,7 +109,9 @@ flashattention_transformer_layer_graph={
     "fused_attention":["q_proj","k_proj","v_proj"],
     "out_proj":["fused_attention"],
     "attn_add":["input","out_proj"],
-    "mlp_norm":["attn_add"],
+    "post_attn_norm":["attn_add"],
+    "gate":["post_attn_norm"],
+    "mlp_norm":["gate"],
     "gate_proj":["mlp_norm"],
     "up_proj":["mlp_norm"],
     "mlp_act":["gate_proj", "up_proj"],
